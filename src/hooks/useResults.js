bcd9c1779a4e77cab180b2dcd440d246d9c06ea7
Lotus Designs <contact.lotusdesigns@gmail.com>
2020-04-20 15:58:13 +0200

    Finnished App from React course o/

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -5,12 +5,12 @@ export default () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
     
-    const searchApi = async () => {
+    const searchApi = async (searchTerm) => {
        try{ 
             const response = await yelp.get('/search', {
                 params: {
                     limit: 50,
-                    term: term,
+                    term: searchTerm,
                     location: "Amsterdam"
                 }
             });
