caf3e97e13a41e45f265376aff9f7d762f61c16b
Jessika <48770521+jessikadg@users.noreply.github.com>
2020-06-06 13:22:06 +0200

    Update yelp.js

--- src/API/yelp.js
+++ src/API/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization: 
-        'Bearer F_6aXRc2thTOga6BVMqhKLvJu8DbTIpRExcSHch7K_uOnAcyq9a-blAqe-AE8C8WFoYzs9hppb1CbJAByKKcys2PIVQs6Mp_yBYEGMrjpG5i8AKsElErP83amouQXnYx'
+        'Bearer INSERT_HERE_YOUR_API_KEY_GENERATED_AT_YELP_YEAH_IT_IS_VERY_LONG_BTW_DONT_FORGET_ONE_DIGIT_SPACE_BETWEEN_BEARER_AND_THE_FIRST_DIGIT_OF_THE_API_KEY'
     }
 }); 
